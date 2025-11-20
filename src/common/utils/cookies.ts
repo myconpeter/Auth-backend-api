84068842df706ad6b5733221c521e88021795ec1
Micheal Peter <michealpeter040@gmail.com>
2025-11-20 08:21:43 +0100

    feat: added swagger docs

--- src/common/utils/cookies.ts
+++ src/common/utils/cookies.ts
@@ -3,44 +3,48 @@ import { config } from '../../config/app.config';
 import { calculateExpirationDate } from './date-time';
 
 type CookiePayLoadType = {
-	res: Response;
-	accessToken: string;
-	refreshToken: string;
+  res: Response;
+  accessToken: string;
+  refreshToken: string;
 };
 
 export const REFRESH_PATH = `${config.BASE_PATH}/auth/refresh`;
 
 const defaults: CookieOptions = {
-	httpOnly: true,
-	secure: config.NODE_ENV === 'production' ? true : false,
-	sameSite: config.NODE_ENV === 'production' ? 'strict' : 'lax',
+  httpOnly: true,
+  secure: config.NODE_ENV === 'production' ? true : false,
+  sameSite: 'none',
 };
 
 export const getRefreshTokenCookieOption = (): CookieOptions => {
-	const expiresIn = config.JWT.JWT_REFRESH_EXPIRES_IN;
-	const expires = calculateExpirationDate(expiresIn);
-	return {
-		...defaults,
-		expires,
-		path: REFRESH_PATH,
-	};
+  const expiresIn = config.JWT.JWT_REFRESH_EXPIRES_IN;
+  const expires = calculateExpirationDate(expiresIn);
+  return {
+    ...defaults,
+    expires,
+    path: REFRESH_PATH,
+  };
 };
 export const getAccessTokenCookieOption = (): CookieOptions => {
-	const expiresIn = config.JWT.EXPIRES_IN;
-	const expires = calculateExpirationDate(expiresIn);
-	return {
-		...defaults,
-		expires,
-		path: '/',
-	};
+  const expiresIn = config.JWT.EXPIRES_IN;
+  const expires = calculateExpirationDate(expiresIn);
+  return {
+    ...defaults,
+    expires,
+    path: '/',
+  };
 };
 
-export const setAuthenticationCookies = ({ res, accessToken, refreshToken }: CookiePayLoadType) =>
-	res
-		.cookie('accessToken', accessToken, getAccessTokenCookieOption())
-		.cookie('refreshToken', refreshToken, getRefreshTokenCookieOption());
+export const setAuthenticationCookies = ({
+  res,
+  accessToken,
+  refreshToken,
+}: CookiePayLoadType) =>
+  res
+    .cookie('accessToken', accessToken, getAccessTokenCookieOption())
+    .cookie('refreshToken', refreshToken, getRefreshTokenCookieOption());
 
 export const clearAuthenticationCookies = (res: Response): Response =>
-	res.clearCookie('accessToken').clearCookie('refreshToken', {
-		path: REFRESH_PATH,
-	});
+  res.clearCookie('accessToken').clearCookie('refreshToken', {
+    path: REFRESH_PATH,
+  });
